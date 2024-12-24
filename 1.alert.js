0ec540e3b4dd2d7ae21c51587abbf74701d93526
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-24 22:32:38 +0530

    first commit

---
+++ 1.alert.js
@@ -0,0 +1,12 @@
+alert("All * Fields are Mandatory");
+alert("Hello sir");
+alert("My Name is ajay");
+alert("I am from MP");
+alert("I think you are fine");
+alert("so you name is vikash");
+alert("you are from satna");
+alert("you are pasuning btech");
+alert("I am a software engineer")
+alert("I get this all information about you")
+alert("so join on monday")
+alert("ob bye")
